f532e63bf08fedc744aa879948302389c3152a32
GrowingPol <growingpol@gmail.com>
2021-09-18 10:26:21 -0600

    Numeration system base int conversion

--- Practice1/Profundizando/Sistemas Numericos.py
+++ Practice1/Profundizando/Sistemas Numericos.py
@@ -4,13 +4,26 @@
 
 #decimal
 a = 10
-print(a)
+
 #Binary
 a = 0b1010
-print(a)
+
 #Octal
 a= 0o12
-print(a)
+
 #Hexadecimal
 a= 0xA
+
+
+#Convertir a un tipo entero, incluyendo base
+a = int('23', 10) #la base por default es 10
+print(a)
+#Base binaria
+a = int('10111',2)
+print(a)
+#Base Octal
+a = int('27',8)
+print(a)
+#Base hexadecimal
+a = int('17',16)
 print(a)
