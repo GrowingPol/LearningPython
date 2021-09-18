e283994579055d7d4089c6f2966532bbacc4af49
GrowingPol <growingpol@gmail.com>
2021-09-18 10:08:13 -0600

    Numeration system

---
+++ Practice1/Profundizando/Sistemas Numericos.py
@@ -0,0 +1,16 @@
+#Profundizando en sistemas de numeracion
+#How to declare values
+#First two leters define whic numeration system it is about
+
+#decimal
+a = 10
+print(a)
+#Binary
+a = 0b1010
+print(a)
+#Octal
+a= 0o12
+print(a)
+#Hexadecimal
+a= 0xA
+print(a)
